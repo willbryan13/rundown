588d693a86dd895352289ec41bfe2c0aa3d755c2
willbryan13 <will.bryan@nousot.com>
2023-01-22 23:33:43 -0600

    Update popup frontend
    Current state summary and areas of further development: everything is current configured to take a user text input, send the input to a backend server at a specified endpoint via http request and the request response returns the processed output. Current issues concern the formatting of the background.js script and what is defined as a service worker, im not sure if these thing are the same but the code for both lives in background.js

--- extension/background.js
+++ extension/background.js
@@ -1,4 +1,14 @@
-// popup.js
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/background.js')
+            .then(registration => {
+                console.log('Service worker registered:', registration);
+            })
+            .catch(registrationError => {
+                console.log('Service worker registration failed:', registrationError);
+            });
+    });
+}
 self.addEventListener('message', event => {
     if (event.data === 'browser-action') {
         var inputText = event.data.input;
