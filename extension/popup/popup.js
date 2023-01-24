e983ce847b4b4f49f8d616c48b58cf205a3743d0
willbryan13 <will.bryan@nousot.com>
2023-01-23 23:14:50 -0600

    Style changes, popup.js update
    Messing with the styling and updating the popup.js to only handle sending the text input to the service worker

--- extension/popup/popup.js
+++ extension/popup/popup.js
@@ -1,29 +1,10 @@
-// popup.js
-self.addEventListener('message', event => {
-    if (event.data === 'browser-action') {
-        var inputText = event.data.input;
-        // send input to the backend server
-        fetch("http://example.com/endpoint", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                input: inputText
-            })
-        })
-            .then(response => {
-                // handle the response
-                if (response.ok) {
-                    return response.json();
-                }
-                throw new Error("Network response was not ok.");
-            })
-            .then(data => {
-                // do something with the data
-            })
-            .catch(error => {
-                // handle errors
-            });
-    }
+document.getElementById("submit-button").addEventListener("click", function () {
+    // Get the input text
+    var inputText = document.getElementById("input-text").value;
+
+    // Send a message to the service worker with the input text
+    navigator.serviceWorker.controller.postMessage({
+        message: 'type',
+        input: inputText
+    });
 });
